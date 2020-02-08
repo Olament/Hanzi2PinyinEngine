4faf2ea04f41db7503c64aa787022c227fb40629
Zixuan Guo <i@zxguo.me>
2020-02-08 15:29:17 -0600

    Initial Commit

---
+++ Pinyin2HanziEngine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinyin2HanziEngine
+//
+//  Created by Zixuan on 2/8/20.
+//  Copyright © 2020 Zixuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
