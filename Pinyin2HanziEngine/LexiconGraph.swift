d9e2124d32243fab0f8be6f07a5dda948ec2da01
Zixuan Guo <i@zxguo.me>
2020-02-15 22:45:23 -0600

    add LexiconGraph LineGraph(SyllableGraph)

--- Pinyin2HanziEngine/LexiconGraph.swift
+++ Pinyin2HanziEngine/LexiconGraph.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class EdgeData {
+    var phrase: String
+    var id: Int
+    var pinyinSequence: PinyinSequence
+    
+    init(phrase: String, id: Int, pinyinSequence: PinyinSequence) {
+        self.phrase = phrase
+        self.id = id
+        self.pinyinSequence = pinyinSequence
+    }
+}
+
+class LexiconGraph: Graph<Int, EdgeData> {
+    init(numberOfVertex count: Int, pinyinSequences: [PinyinSequence], tree: LexiconTree) {
+        super.init(numberOfVertices: count)
+        
+        for sequence in pinyinSequences {
+            if let phrases = tree.searchPhrase(pinyins: sequence.pinyinSequence) {
+                for phrase in phrases {
+                    let edge = addEdge(from: self.vertices[sequence.from], to: self.vertices[sequence.to])
+                    edge.data = EdgeData(phrase: phrase, id: self.edgeCount, pinyinSequence: sequence) // add id in sequence
+                }
+            }
+        }
+    }
+}
