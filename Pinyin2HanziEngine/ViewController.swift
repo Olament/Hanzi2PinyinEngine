c596159a18f6f5293b7bd9687c8f5d4d9288eb60
Zixuan Guo <i@zxguo.me>
2020-02-17 21:57:54 -0600

    add data loading for lexiconTree and construct lexicon graph

--- Pinyin2HanziEngine/ViewController.swift
+++ Pinyin2HanziEngine/ViewController.swift
@@ -13,9 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // init data file
+        ///data loading
+        // init syllable
         var syllableSet: Set<String> = Set()
         if let path = Bundle.main.path(forResource: "syllable", ofType: "txt") {
             do {
@@ -29,11 +28,31 @@ class ViewController: UIViewController {
             }
         }
         
-        
-        let syllableGraph = SyllableGraph(pinyin: "nihao", validSyllable: syllableSet)
-        for sequence in syllableGraph.getPinyinSequence(maxLength: 6) {
-            print(sequence.pinyinSequence)
+        // init Lexicon Tree
+        let lexiconTree: LexiconTree = LexiconTree()
+        if let path = Bundle.main.path(forResource: "lexicon", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let lines = data.split(separator:"\n")
+                for word in lines {
+                    let parts = word.split(separator: "\t")
+                    let phrase = String(parts[1])
+                    let pinyins = parts[0].split(separator: "'").map(String.init)
+                    print(pinyins)
+                    lexiconTree.insertPhrase(phrase: phrase, pinyins: pinyins)
+                }
+            } catch {
+                print(error)
+            }
         }
+        
+        // init syllable graph
+        let syllableGraph = SyllableGraph(pinyin: "nihao", validSyllable: syllableSet)
+        
+        // init lexicon graph
+        let lexiconGraph = LexiconGraph(numberOfVertex: syllableGraph.vertexCount,
+                                        pinyinSequences: syllableGraph.getPinyinSequence(maxLength: 6),
+                                        tree: lexiconTree)
     }
 }
 
