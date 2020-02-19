2afcd72611d669d5cd6487b80387bcae886d60af
Zixuan Guo <i@zxguo.me>
2020-02-18 22:39:51 -0600

    add UI

--- Pinyin2HanziEngine/ViewController.swift
+++ Pinyin2HanziEngine/ViewController.swift
@@ -12,9 +12,30 @@ import Foundation
 class ViewController: UIViewController {
     
     let model: PinyinEngine = PinyinEngine()
-
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var output: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func getSentence(_ sender: UIButton) {
+        if let inputString = input.text {
+            let solutions = model.getSentence(pinyin: inputString)
+            
+            output.text = ""
+            for solution in solutions {
+                output.text.append("\(solution.sentence) \(solution.probability)\n")
+            }
+        }
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        input.resignFirstResponder()
     }
 }
 
