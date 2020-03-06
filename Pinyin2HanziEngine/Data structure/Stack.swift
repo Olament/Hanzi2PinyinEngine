c2eb3a4956cb71919d9d41b37be6f49aff80b058
Zixuan Guo <i@zxguo.me>
2020-03-05 19:49:02 -0600

    add Stack implementation

---
+++ Pinyin2HanziEngine/Data structure/Stack.swift
@@ -0,0 +1,46 @@
+//
+//  LinkedList.swift
+//  Pinyin2HanziEngine
+//
+//  Created by Zixuan on 3/5/20.
+//  Copyright © 2020 Zixuan. All rights reserved.
+//
+
+import Foundation
+
+class Stack<E> {
+    private class Node<E> {
+        var item: E
+        var next: Node?
+        
+        init(item: E, next: Node?) {
+            self.item = item
+            self.next = next
+        }
+    }
+    
+    private var head: Node<E>?
+    
+    var count = 0
+    
+    func pop() -> E {
+        let returnElement: E = self.head!.item
+        self.head!.next = nil // GC
+        self.head = self.head?.next
+        
+        count -= 1
+        
+        return returnElement
+    }
+    
+    func push(newElement: E) {
+        let newNode = Node(item: newElement, next: self.head)
+        self.head = newNode
+        
+        count += 1
+    }
+    
+    func isEmpty() -> Bool {
+        return count == 0
+    }
+}
